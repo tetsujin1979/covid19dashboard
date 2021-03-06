8b78230b659c610b7dda6b7ac9642f1aea17c362
tetsujin1979 <tetsujin1979@gmail.com>
2021-03-06 17:04:29 +0000

    Update data.js
    Added swab data for Saturday, 6th March 2021

--- data.js
+++ data.js
@@ -369,5 +369,6 @@ const data = [
   {date: new Date('2021-03-02'), positiveSwabs: 423, dailySwabs: 11650, cases: 359, deaths: 14, firstDose: 12506, secondDose: 1657},
   {date: new Date('2021-03-03'), positiveSwabs: 673, dailySwabs: 15277, cases: 566, deaths: 25, firstDose: 12542, secondDose: 1466},
   {date: new Date('2021-03-04'), positiveSwabs: 552, dailySwabs: 14244, cases: 462, deaths: 39},
-  {date: new Date('2021-03-05'), positiveSwabs: 615, dailySwabs: 16640, cases: 522, deaths: 9}
+  {date: new Date('2021-03-05'), positiveSwabs: 615, dailySwabs: 16640, cases: 522, deaths: 9},
+  {date: new Date('2021-03-06'), positiveSwabs: 460, dailySwabs: 12862}
 ];
