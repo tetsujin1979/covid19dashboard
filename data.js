f314d3bd546eb1d3831ac4ac7b004ecd253039fb
tetsujin1979 <tetsujin1979@gmail.com>
2021-03-09 11:31:42 +0000

    Update data.js
    Added vaccine dose data for Saturday, 6th March 2021

--- data.js
+++ data.js
@@ -370,7 +370,7 @@ const data = [
   {date: new Date('2021-03-03'), positiveSwabs: 673, dailySwabs: 15277, cases: 566, deaths: 25, firstDose: 12542, secondDose: 1466},
   {date: new Date('2021-03-04'), positiveSwabs: 552, dailySwabs: 14244, cases: 462, deaths: 39, firstDose: 17658, secondDose: 1570},
   {date: new Date('2021-03-05'), positiveSwabs: 615, dailySwabs: 16640, cases: 522, deaths: 9, firstDose: 17345, secondDose: 2104},
-  {date: new Date('2021-03-06'), positiveSwabs: 460, dailySwabs: 12862, cases: 539, deaths: 14},
+  {date: new Date('2021-03-06'), positiveSwabs: 460, dailySwabs: 12862, cases: 539, deaths: 14, firstDose: 9548, secondDose: 199},
   {date: new Date('2021-03-07'), positiveSwabs: 445, dailySwabs: 13741, cases: 525, deaths: 3},
   {date: new Date('2021-03-08'), positiveSwabs: 464, dailySwabs: 10675, cases: 437, deaths: 0}
 ];
