5eef4dfd5c486c941a3791b204c2c7318d9eb47e
tetsujin1979 <tetsujin1979@gmail.com>
2021-03-10 17:49:47 +0000

    Update data.js
    Added case and death data for Wednesday, 10th March 2021

--- data.js
+++ data.js
@@ -374,6 +374,6 @@ const data = [
   {date: new Date('2021-03-07'), positiveSwabs: 445, dailySwabs: 13741, cases: 525, deaths: 3, firstDose: 2372, secondDose: 327},
   {date: new Date('2021-03-08'), positiveSwabs: 464, dailySwabs: 10675, cases: 437, deaths: 0},
   {date: new Date('2021-03-09'), positiveSwabs: 414, dailySwabs: 12330, cases: 311, deaths: 30},
-  {date: new Date('2021-03-10'), positiveSwabs: 600, dailySwabs: 15278}
+  {date: new Date('2021-03-10'), positiveSwabs: 600, dailySwabs: 15278, cases: 631, deaths: 47}
   
 ];
