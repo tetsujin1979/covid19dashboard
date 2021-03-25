f3940c5e5387ad99d17fcfcda2d18b0d3012e299
tetsujin1979 <tetsujin1979@gmail.com>
2021-03-25 19:55:47 +0000

    Update data.js
    Added case and death data for Thursday, 25th March 2021

--- data.js
+++ data.js
@@ -389,5 +389,5 @@ const data = [
   {date: new Date('2021-03-22'), positiveSwabs: 506, dailySwabs: 12722, cases: 520, deaths: 1, firstDose: 7972, secondDose: 2462},
   {date: new Date('2021-03-23'), positiveSwabs: 520, dailySwabs: 15044, cases: 379, deaths: 24},
   {date: new Date('2021-03-24'), positiveSwabs: 694, dailySwabs: 19979, cases: 683, deaths: 18},
-  {date: new Date('2021-03-25'), positiveSwabs: 604, dailySwabs: 17821}
+  {date: new Date('2021-03-25'), positiveSwabs: 604, dailySwabs: 17821, cases: 606, deaths: 3}
 ];
