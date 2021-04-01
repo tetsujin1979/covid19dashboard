2cea26354b8aa41b6e0feddb6b24b501a36ed9ed
tetsujin1979 <tetsujin1979@gmail.com>
2021-04-01 15:46:40 +0100

    Update data.js
    Added swab data for Thursday, 1st April 2021

--- data.js
+++ data.js
@@ -395,5 +395,6 @@ const data = [
   {date: new Date('2021-03-28'), positiveSwabs: 603, dailySwabs: 19619, cases: 604, deaths: 13, firstDose: 3216, secondDose: 823},
   {date: new Date('2021-03-29'), positiveSwabs: 498, dailySwabs: 13507, cases: 539, deaths: 1, firstDose: 9831, secondDose: 3304},
   {date: new Date('2021-03-30'), positiveSwabs: 385, dailySwabs: 14729, cases: 368, deaths: 14},
-  {date: new Date('2021-03-31'), positiveSwabs: 712, dailySwabs: 21051, cases: 411, deaths: 6}
+  {date: new Date('2021-03-31'), positiveSwabs: 712, dailySwabs: 21051, cases: 411, deaths: 6},
+  {date: new Date('2021-04-01'), positiveSwabs: 553, dailySwabs: 19535}
 ];
